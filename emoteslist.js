132937e14b6f09c9ac1d3d644d3161f34b0041a3
Goldenegg000 <74820527+Goldenegg000@users.noreply.github.com>
2021-09-25 13:39:16 +0200

    some more testing

---
+++ emoteslist.js
@@ -0,0 +1,9 @@
+var MyEmotePicker = injectHTML("TheCostumeEmotesTag", `
+    <h2>testing</h2>
+    <hr>
+    <p>test emote here</p>
+    <p>test emote here</p>
+    <p>test emote here</p>
+    <p>test emote here</p>
+    <p>test emote here</p>
+`)
