0ed48e04a28d4c8037298f4b6bfe462f81a87b9f
Goldenegg000 <74820527+Goldenegg000@users.noreply.github.com>
2021-09-28 14:25:40 +0200

    hummm

--- testing.js
+++ testing.js
@@ -1,1 +1,16 @@
-alert('test')
+AddNewCostumeEmote("BigChamp", "png");
+AddNewCostumeEmote("Eman", "png");
+AddNewCostumeEmote("GE", "png");
+AddNewCostumeEmote("CoolPepe", "png");
+AddNewCostumeEmote("Countdown", "gif");
+AddNewCostumeEmote("Pepedance", "gif");
+AddNewCostumeEmote("Pepesaber", "gif");
+AddNewCostumeEmote("PogChamp", "png");
+AddNewCostumeEmote("Wha", "gif");
+AddNewCostumeEmote("emerald", "png");
+AddNewCostumeEmote("image", "png");
+AddNewCostumeEmote("Bits1", "gif");
+AddNewCostumeEmote("Bits100", "gif");
+AddNewCostumeEmote("Bits1000", "gif");
+AddNewCostumeEmote("Bits5000", "gif");
+AddNewCostumeEmote("Bits10000", "gif");
